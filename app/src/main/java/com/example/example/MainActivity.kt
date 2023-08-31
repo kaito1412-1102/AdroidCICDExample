60dc88c4c4947e954196798eb1f72bc60dd86ee1
Minh <kaito1412.1102@gmail.com>
2023-08-31 14:13:29 +0700

    feat. import app center lib

--- app/src/main/java/com/example/example/MainActivity.kt
+++ app/src/main/java/com/example/example/MainActivity.kt
@@ -2,15 +2,25 @@ package com.example.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.example.databinding.ActivityMainBinding
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         AppCenter.start(application, "3e262042-852f-41bc-8b2f-6abc688717ea", Analytics::class.java, Crashes::class.java)
+
+        binding.btnCrash.setOnClickListener {
+//            throw Exception("Something went wrong")
+//            Crashes.generateTestCrash()
+            Analytics.trackEvent("wrong button")
+        }
     }
 }
