9c959da16fdd256a3c33bd50a5b26a8e51d49f65
Minh <kaito1412.1102@gmail.com>
2023-08-31 10:47:37 +0700

    feat. import app center lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+//    def appCenterSdkVersion = "4.4.5"
+    implementation("com.microsoft.appcenter:appcenter-analytics:4.4.5")
+    implementation("com.microsoft.appcenter:appcenter-crashes:4.4.5")
 }
--- app/src/main/java/com/example/example/MainActivity.kt
+++ app/src/main/java/com/example/example/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.example
 
-import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(application, "3e262042-852f-41bc-8b2f-6abc688717ea", Analytics::class.java, Crashes::class.java)
     }
 }
